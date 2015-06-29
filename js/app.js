feaee160c7c88251627a4177ed870ed4be8a85c1
Alain Cidrera <alain.cidrera@tech-impulse.com>
2015-06-29 13:18:00 +0200

    pagina de inicio e iconos

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@ $(document).ready(function () {
     $("#btnCatalogo").bind("click", function (event, ui) {
         $("body").pagecontainer("change", "#catalogo");
     });
+    $("#btnFiesta").bind("click", function (event, ui) {
+    $("body").pagecontainer("change", "#fiesta");
+    });
+    $("#btnDisfraz").bind("click", function (event, ui) {
+        $("body").pagecontainer("change", "#disfraz");
+    });
 });
 
 //
