402cbd1cc2019c7c000fb15941d297479a336780
Alberto-tech <alberto.alarcon@tech-impulse.com>
2016-02-25 13:13:42 +0100

    Retoques

--- js/rest.js
+++ js/rest.js
@@ -1442,8 +1442,6 @@ function sendEmail() {
 
                 }
 
-
-
             },
             error: function (jqXHR, textStatus, errorThrown) {
 
@@ -1464,10 +1462,7 @@ function sendEmail() {
                 INFO_USU = "";
             },
         });
-
     }
-
-
 }
 
 /**************************************************************************
@@ -1479,14 +1474,12 @@ function imprimirPedido() {
         carrito: CART,
         tienda: STORE.code,
         currencySymbol: STORE.currencySymbol,
-        lang:language
+        lang: language
     };
 
     var request = $.ajax({
         data: dataSend,
-        //async: false,
         url: urlServices + 'convert_pdf.php',
-        //dataType: 'json',
         type: 'POST',
         success: function (response) {
 
@@ -1586,147 +1579,4 @@ function imprimirPedido() {
         }
     });
 
-    //}
-
-
 }
-<<<<<<< HEAD
-=======
-
-/**
-*   pedidoOnline
-*
-*   funcion que envia los datos del carrito al webservice de pedido online
-*/
-function pedidoOnline()    {
-    
-    var dataSend = {
-        carrito: CART,
-        tienda: STORE.code
-    };
-
-    var request = $.ajax({
-        data: dataSend,
-        //async: false,
-        url: urlServices + 'pedidoOnline.php',
-        //dataType: 'json',
-        type: 'POST',
-        success: function (response) {
-
-            console.log("Respuesta de pedidoOnline es:");
-            console.log(response);
-            
-            //temp para puesta en tienda
-            $("#texto_popup").text("Pedido enviado online");
-                EMAIL_USER = "";
-                INFO_USU = "";
-                $('#popupAlert').popup('open');
-                $('#email').val('');
-                $("#spBtnAmountPerson").text('');
-                $("#circuloCantidad").hide();
-                $("#spBtnPopupCartAmmount").hide();
-                $("#userIcoCarrito").hide();
-                $("#btn_finalizarpedido").hide();
-                CART = [];
-                nodeNames = [];
-                nodeIds = [];
-                nodeImg = [];
-                EMAIL_USER = "";
-                logout();
-                console.log("Enviamos el pedido online");
-
-
-                setTimeout(function () {
-                    $('#popupAlert').popup('close');
-                    getNodes(0);
-                }, 1500);
-
-            if (parseInt(response.result) == parseInt(1)) {
-
-                /*$("#texto_popup").text("Pedido enviado online");
-                EMAIL_USER = "";
-                INFO_USU = "";
-                $('#popupAlert').popup('open');
-                $('#email').val('');
-                $("#spBtnAmountPerson").text('');
-                $("#circuloCantidad").hide();
-                $("#spBtnPopupCartAmmount").hide();
-                $("#userIcoCarrito").hide();
-                $("#btn_finalizarpedido").hide();
-                CART = [];
-                nodeNames = [];
-                nodeIds = [];
-                nodeImg = [];
-                EMAIL_USER = "";
-                logout();
-                console.log("Enviamos el pedido online");*/
-
-                setTimeout(function () {
-                    $('#popupAlert').popup('close');
-                    getNodes(0);
-                }, 1500);
-
-            } else if (parseInt(response.result) == parseInt(0)) {      // ***** Cambiar en funcion del webservice a utilizar !!!!! ****
-
-                $("#texto_popup").text("No se ha podido enviar el pedido");
-                $('#popupAlert').popup('open');
-
-            } else if (parseInt(response.result) == parseInt(2)) {
-
-                $("#texto_popup").text("Problemas al generar el pedido");
-                $('#popupAlert').popup('open');
-
-            } else if (parseInt(response.result) == parseInt(0)) {
-
-                $("#texto_popup").text("Faltan datos para poder enviar el pedido");
-                $('#popupAlert').popup('open');
-
-            }
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-
-            if (textStatus === "timeout") {
-
-                console.log("Timeout");
-                alert("Error de TimeOut... compruebe su conexion de internet");
-
-            } else {
-
-                //restError(jqXHR, "tiendas");
-                console.log("Sin conexion");
-                //alert("Sin conexion a internet...");
-                $("#texto_popup").text("Sin conexion a internet");
-                $('#popupAlert').popup('open');
-
-            }
-
-        }
-    });
-}
-
-/**
-*   cancelaPedido
-*
-*   funcion que vacia el carrito, vuelve a la pantalla inicial y esta logado, hace un logout.
-*/
-function cancelaPedido()    {
-    vaciaCarrito();
-    
-    if ( EMAIL_USER != "" )   {
-        EMAIL_USER = "";
-    }
-    
-    INFO_USU = "";
-    $('#popupAlert').popup('open');
-    $('#email').val('');
-    $("#spBtnAmountPerson").text('');
-    $("#circuloCantidad").hide();
-    $("#spBtnPopupCartAmmount").hide();
-    $("#userIcoCarrito").hide();
-    $("#btn_finalizarpedido").hide();
-    CART = [];
-    nodeNames = [];
-    nodeIds = [];
-    nodeImg = [];
-}
->>>>>>> origin/master
