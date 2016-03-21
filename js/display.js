c64467b0266faaa0d40ba7406941aab2a68c8df8
jordipasmi <jordipascualmilego@gmail.com>
2016-03-21 13:45:28 +0100

    Gastos envio

--- js/display.js
+++ js/display.js
@@ -4372,19 +4372,19 @@ function opcionesEnvio(casoEnvio, totalCesta) { //TEMP
             '</div>' +
             '<div class="ui-grid-a">' +
             '<div class="ui-block-a" style="float:left;"><label></label></div>' +
-            '<div class="ui-block-b" style="float:rigth;text-align: right;width:100%;"><label>Total cesta: '+ totalCesta +'€ + gastos de envio = 30,25€</label></div>' +
+            '<div class="ui-block-b" style="float:rigth;text-align: right;width:100%;"><label>Total cesta: <strong>'+ totalCesta +'€</strong> + gastos de envio (4,99€)<br>(gratuito a partir de 30€ con entrega en tienda o<br> a partir de 75€ con entrega a domicilio)</label></div>' +
             '</div>' +
             '</a>' +
             '<a data-corners="false" style="width:600px" onclick="formularioTiendaDestino()" data-role="button" data-theme="b" >' +
             '<div class="ui-grid-a">' +
-            '<div class="ui-block-a"><label>CLICK AND COLLECT 48H</label></div>' +
+            '<div class="ui-block-a" style="text-align: left;"><label>CLICK AND COLLECT 48H</label></div>' +
             '</div>' +
             '<div class="ui-grid-a">' +
             '<div class="ui-block-a" style="float:left;"><label></label></div>' +
 
             //'<div class="ui-block-b" style="text-align: right;width:100%;"><label>Total cesta: 25.23€ + gastos de envio = 30,25€</label></div>' +
 
-            '<div class="ui-block-b" style="float:rigth; text-align: right;width:100%;"><label>Total cesta: '+ totalCesta +'€ + gastos de envio = 30,25€</label></div>' +
+            '<div class="ui-block-b" style="float:rigth; text-align: right;width:100%;"><label>Total cesta: <strong>'+ totalCesta +'€</strong> + gastos de envio (4,99€)<br>(gratuito a partir de 30€ con entrega en tienda o<br> a partir de 75€ con entrega a domicilio)</label></div>' +
 
             '</div>' +
             '</a>' +
