cd425abc9df93fdfd24b30421d69d0dc61c181ea
Alberto-tech <alberto.alarcon@tech-impulse.com>
2016-06-14 10:39:43 +0200

    cambiar tiempo screen

--- js/config.js
+++ js/config.js
@@ -25,7 +25,7 @@ var node_cero = [];
 var LOGGED = false;
 var REDIRECT = false;
 var popupTimeout = 150;
-var idleTime = 5000; /// tiempo de inactividad
+var idleTime = 180000; /// tiempo de inactividad
 var idleTimeActive = false;
 var idiomStore;
 var protector;
