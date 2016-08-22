564597a8b41932752d29065d7cc13328a6bc67f7
jordipasmi <jordipascualmilego@gmail.com>
2016-08-22 12:41:16 +0200

    Cambio url webservices hacia nuevo servidor

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // AQUI VAN DEFINIDAS LAS VARIABLES QUE SE USARÁN MAS DE UNA VEZ EN MÁS DE UN FICHERO
-var urlServices = "http://partyfiesta.youtter.com/webservices/"; // URL del servidor de Webservices fuera de creapolis
-//var urlServices = "http://partyfiesta-prod.youtter.com/webservices/"; // URL del servidor de Webservices
+//var urlServices = "http://partyfiesta.youtter.com/webservices/"; // URL del servidor de Webservices fuera de creapolis
+var urlServices = "http://partyfiesta-prod.youtter.com:60780/webservices/"; // URL del servidor de Webservices
 var request;
 var language = 2; // El idioma de la aplicación por defecto demomento CATALAN
 var origin = 2; // Dispositivo desde el que se solicita la información al service: 1 Web, 2 App
